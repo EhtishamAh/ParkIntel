5503299edc7b951a05d365c3c7f2ecfda81623ea
Ehtisham Ahmed <ehtishamahmed551@gmail.com>
2025-12-03 16:45:06 +0500

    Errors Fix

--- ADD_USER_ID_TO_PRE_BOOKINGS.sql
+++
@@ -1,8 +0,0 @@
--- Add reservation_fee column to pre_bookings table
--- This stores the 20% pre-booking fee charged to the user
-
-ALTER TABLE public.pre_bookings 
-ADD COLUMN IF NOT EXISTS reservation_fee numeric;
-
--- Add comment for documentation
-COMMENT ON COLUMN public.pre_bookings.reservation_fee IS 'The 20% reservation fee charged for pre-booking (in PKR)';
